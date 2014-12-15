c5704b1167ed6d00fdb704e4590e4704ea7e008d
k8hertweck <k8hertweck@gmail.com>
2014-12-15 09:57:51 -0600

    adding export to .odt with pandoc

--- syllabus.sh
+++ syllabus.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-##Concatenate syllabus using order of modules in syllabus.lst
+##Make syllabus out of modules
+##dependencies: pandoc (http://johnmacfarlane.net/pandoc/)
 ##syntax: syllabus.sh syllabus
 ##output: syllabus.md
 
+#Concatenate syllabus using order of modules in syllabus.lst
 cat $(grep -v '^#' $1.lst) > $1.md
+
+#Convert from .md to .docx using pandoc
+pandoc $1.md -o $1.odt
